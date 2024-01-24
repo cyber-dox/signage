c7a19e94f5541da4bc2a7fc81052081dad3583a5
Tahir-Ahmad-Wani <wanitahir59@gmail.com>
2024-01-24 23:52:10 +0530

    version 1

---
+++ index.js
@@ -0,0 +1,8 @@
+header = document.createElement("h1");
+header.innerHTML = "Hello World!";
+document.body.appendChild(header);
+header.id = "hd1";
+marquee = document.createElement("marquee");
+marquee.innerHTML = "Learn JavaScript";
+document.body.appendChild(marquee);
+marquee.id="mq1";
