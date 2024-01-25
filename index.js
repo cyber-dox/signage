0f19ccc75b165456a86d67105fbfb44d83720167
Tahir-Ahmad-Wani <wanitahir59@gmail.com>
2024-01-25 22:44:34 +0530

    version 1

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ header = document.createElement("h1");
 header.innerHTML = "Hello World!";
 document.body.appendChild(header);
 header.id = "hd1";
+
 marquee = document.createElement("marquee");
 marquee.innerHTML = "Learn JavaScript";
 document.body.appendChild(marquee);
 marquee.id="mq1";
+
 marquee2 = document.createElement("marquee");
 marquee2.innerHTML = "Marquee 2";
 document.body.appendChild(marquee2);
@@ -13,3 +15,7 @@ document.body.appendChild(marquee2);
 division = document.createElement("div");
 division.id = "div1";
 document.body.appendChild(division);
+
+circle = document.createElement("div");
+circle.id = "cir1";
+document.body.appendChild(circle);
